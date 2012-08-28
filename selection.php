7bf663375ade742fdeb8b89c5e54a3356cbaba47
Dmitry Kurilo <me@masterrr.name>
2012-08-29 02:42:43 +0300

    Caching was added, constructor, refactoring, new comments

--- selection.php
+++ selection.php
@@ -1,46 +1,31 @@
 <?php
+
 class User {
-	private $uid = '';
-	function set_id($id) {
+	private $uid = NULL;
+	static $data = NULL;
+
+	function __construct($id) {
 		$this->uid = $id;
 	}
 	
-	function getuserinfo() {
-	
-		#DB Connection
-		$conn = DB_Instance::getDBO();
+	# Gathering how many time user has spent, and when he has registered
+	function getuserinfo() { 
+		$conn = DB_Instance::getDBO(); #DB Connection
 		
-		# Gathering how many time user has spent, and when he has registered
-		$query1 = $conn->prepare("SELECT spent, date FROM `users` WHERE `uid`=?");
-		$result1 = $query1->execute(array($this->uid));
-		$data1 = $query1->fetch(PDO::FETCH_NUM);
+		if (!$this->data) { # Caching
+			$query = $conn->prepare("SELECT spent, date FROM `users` WHERE `uid`=?");
+			$result = $query->execute(array($this->uid));
+			$this->data = $query->fetch(PDO::FETCH_NUM);			
+		}
 		return array(
-			'spent' => $data1[0], # Spent time in seconds
-			'datetime' => $data1[1] # Registering datetime in 2012-02-04 22:59:55 format
+			'spent' => (int)($this->data[0]), # Spent time in seconds
+			'datetime' => $this->data[1] # Registering datetime in 2012-02-04 22:59:55 format
 		);
-		unset($query1, $result1, $data1);
-		
-	}
-	function get_spent() {
-		$userinfo = $this->getuserinfo();		
-		return $userinfo['spent'];
+		unset($query, $result, $data);	
 	}
 	
-	function get_datetime() {
-		$userinfo = $this->getuserinfo();		
-		return $userinfo['datetime'];
-	}
-	
-	function get_registration_timestamp() {
-		$userinfo = $this->getuserinfo();	
-		$timestamp = strtotime($userinfo['datetime']); # Registering datetime timestamp
-		return $timestamp;	
-	}
-	
-	function get_time_passed_timestamp() {
-		$timestamp = $this -> get_registration_timestamp();	
-		$time_passed = time() - $timestamp; # Seconds after registration
-		return $time_passed;
-	
-	}
+	function get_spent() { $userinfo = $this->getuserinfo(); return $userinfo['spent']; } # Spent time in seconds (timestamp)
+	function get_datetime() { $userinfo = $this->getuserinfo();	return $userinfo['datetime']; } # Datetime (2012-02-04 22:59:55)
+	function get_registration_timestamp() { $timestamp = strtotime($this->get_datetime()); return $timestamp; } # When registered (timestamp)	
+	function get_time_passed_timestamp() { $timestamp = $this -> get_registration_timestamp(); return time() - $timestamp; } # Now - when registered (timestamp)
 }

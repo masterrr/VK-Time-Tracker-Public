e507e69fc3e9eaeeb52702f6a2ff538e68818837
Dmitry Kurilo <me@masterrr.name>
2012-08-29 01:36:13 +0300

    without static, man!

--- selection.php
+++ selection.php
@@ -1,21 +1,25 @@
 <?php
 class User {
+	private $uid = '';
+	function set_id($id) {
+		$this->uid = $id;
+	}
 	
-	function getuserinfo($uid) {
+	function getuserinfo() {
 	
 		#DB Connection
 		$conn = DB_Instance::getDBO();
 		
 		# Gathering how many minutes user has spent.
 		$query1 = $conn->prepare("SELECT spent FROM `users` WHERE `uid`=?");
-		$result1 = $query1->execute(array($uid));
+		$result1 = $query1->execute(array($this->uid));
 		$data1 = $query1->fetch(PDO::FETCH_NUM);
 		$spent = (int)$data1[0]; # How many minutes user has spent.
 		unset($query1, $result1, $data1);
 		
 		# From what date on our service?
 		$query1 = $conn->prepare("SELECT date FROM `users` WHERE `uid`=?");
-		$result1 = $query1->execute(array($uid));
+		$result1 = $query1->execute(array($this-uid));
 		$data1 = $query1->fetch(PDO::FETCH_NUM);
 		$datetime = $data1[0]; # Registering datetime in 2012-02-04 22:59:55 format
 		unset($query1, $result1, $data1);
@@ -25,25 +29,24 @@ class User {
 		);
 		
 	}
-	
-	function get_spent($uid) {
-		$userinfo = User::getuserinfo($uid);		
+	function get_spent() {
+		$userinfo = $this->getuserinfo();		
 		return $userinfo['spent'];
 	}
 	
-	function get_datetime($uid) {
-		$userinfo = User::getuserinfo($uid);		
+	function get_datetime() {
+		$userinfo = $this->getuserinfo();		
 		return $userinfo['datetime'];
 	}
 	
-	function get_registration_timestamp($uid) {
-		$userinfo = User::getuserinfo($uid);	
+	function get_registration_timestamp() {
+		$userinfo = $this->getuserinfo();	
 		$timestamp = strtotime($datatime); # Registering datetime timestamp
 		return $timestamp;	
 	}
 	
-	function get_time_passed_timestamp($uid) {
-		$timestamp = User::get_registration_timestamp($uid);	
+	function get_time_passed_timestamp() {
+		$timestamp = $this -> get_registration_timestamp();	
 		$time_passed = time() - $timestamp; # Seconds after registration
 		return $time_passed;
 	

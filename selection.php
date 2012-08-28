5af775aabbc3258862898dad824fe3bf5b9f792c
Dmitry Kurilo <me@masterrr.name>
2012-08-29 00:26:43 +0300

    return

--- selection.php
+++ selection.php
@@ -11,7 +11,7 @@ class Selection {
 		$data1 = $query1->fetch(PDO::FETCH_NUM);
 		$spent = (int)$data1[0]; # How many minutes user has spent.
 		unset($query1, $result1, $data1);
-
+		
 		# From what date on our service?
 		$query1 = $conn->prepare("SELECT date FROM `users` WHERE `uid`=?");
 		$result1 = $query1->execute(array($uid));
@@ -20,6 +20,18 @@ class Selection {
 		unset($query1, $result1, $data1);
 		$timestamp = strtotime($datatime); # Registering datetime timestamp
 		$time_passed = time() - $timestamp; # Seconds after registration
-		# var_dump($spent, $datatime, $timestamp, $time_passed);
-	}
+		$userinfo = array (
+			'datatime' => $datatime,
+			'timestamp_registration' => $timestamp,
+			'timestamp_passed' => $time_passed
+		);
+		return $userinfo;
+		/* 
+		array(3) { 
+			["datatime"]=> string(19) "2012-02-04 22:59:55" 
+			["timestamp_registration"]=> int(1328389195) 
+			["timestamp_passed"]=> int(17799910) 
+		}	
+		*/
+		}
 }

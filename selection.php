bdfd3fbea8aeeb707a657cf53d1f3709aa8c32c3
Dmitry Kurilo <me@masterrr.name>
2012-08-29 01:15:04 +0300

    for static

--- selection.php
+++ selection.php
@@ -1,6 +1,7 @@
 <?php
-class Selection {
-	function getusertime($uid) {
+class User {
+	
+	function getuserinfo($uid) {
 	
 		#DB Connection
 		$conn = DB_Instance::getDBO();
@@ -16,23 +17,35 @@ class Selection {
 		$query1 = $conn->prepare("SELECT date FROM `users` WHERE `uid`=?");
 		$result1 = $query1->execute(array($uid));
 		$data1 = $query1->fetch(PDO::FETCH_NUM);
-		$datatime = $data1[0]; # Registering datetime in 2012-02-04 22:59:55 format
+		$datetime = $data1[0]; # Registering datetime in 2012-02-04 22:59:55 format
 		unset($query1, $result1, $data1);
-		$timestamp = strtotime($datatime); # Registering datetime timestamp
-		$time_passed = time() - $timestamp; # Seconds after registration
-		$userinfo = array (
-			'datatime' => $datatime,
-			'timestamp_registration' => $timestamp,
-			'timestamp_passed' => $time_passed
+		return array(
+			'spent' => $spent,
+			'datetime' => $datetime
 		);
-		return $userinfo;
-		# Returning:
-		/* 
-		array(3) { 
-			["datatime"]=> string(19) "2012-02-04 22:59:55" 
-			["timestamp_registration"]=> int(1328389195) 
-			["timestamp_passed"]=> int(17799910) 
-		}	
-		*/
+		
+	}
+	
+	function get_spent($uid) {
+		$userinfo = User::getuserinfo($uid);		
+		return $userinfo['spent'];
+	}
+	
+	function get_datetime($uid) {
+		$userinfo = User::getuserinfo($uid);		
+		return $userinfo['datetime'];
+	}
+	
+	function get_registration_timestamp($uid) {
+		$userinfo = User::getuserinfo($uid);	
+		$timestamp = strtotime($datatime); # Registering datetime timestamp
+		return $timestamp;	
+	}
+	
+	function get_time_passed_timestamp($uid) {
+		$timestamp = User::get_registration_timestamp($uid);	
+		$time_passed = time() - $timestamp; # Seconds after registration
+		return $time_passed;
+	
 	}
 }

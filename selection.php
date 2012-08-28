4000e62721795be6dbf88dc768a94aaa009e7f62
Dmitry Kurilo <me@masterrr.name>
2012-08-29 02:09:47 +0300

    Merje 2 SQL queries into one

--- selection.php
+++ selection.php
@@ -10,23 +10,15 @@ class User {
 		#DB Connection
 		$conn = DB_Instance::getDBO();
 		
-		# Gathering how many minutes user has spent.
-		$query1 = $conn->prepare("SELECT spent FROM `users` WHERE `uid`=?");
+		# Gathering how many time user has spent, and when he has registered
+		$query1 = $conn->prepare("SELECT spent, date FROM `users` WHERE `uid`=?");
 		$result1 = $query1->execute(array($this->uid));
 		$data1 = $query1->fetch(PDO::FETCH_NUM);
-		$spent = (int)$data1[0]; # How many minutes user has spent.
-		unset($query1, $result1, $data1);
-		
-		# From what date on our service?
-		$query1 = $conn->prepare("SELECT date FROM `users` WHERE `uid`=?");
-		$result1 = $query1->execute(array($this-uid));
-		$data1 = $query1->fetch(PDO::FETCH_NUM);
-		$datetime = $data1[0]; # Registering datetime in 2012-02-04 22:59:55 format
-		unset($query1, $result1, $data1);
 		return array(
-			'spent' => $spent,
-			'datetime' => $datetime
+			'spent' => $data1[0], # Spent time in seconds
+			'datetime' => $data1[1] # Registering datetime in 2012-02-04 22:59:55 format
 		);
+		unset($query1, $result1, $data1);
 		
 	}
 	function get_spent() {
@@ -41,7 +33,7 @@ class User {
 	
 	function get_registration_timestamp() {
 		$userinfo = $this->getuserinfo();	
-		$timestamp = strtotime($datatime); # Registering datetime timestamp
+		$timestamp = strtotime($userinfo['datetime']); # Registering datetime timestamp
 		return $timestamp;	
 	}
 	

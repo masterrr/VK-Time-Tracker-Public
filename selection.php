816bce625eab57ecf0e0cb362ad5f575231dba71
Dmitry Kurilo <me@masterrr.name>
2012-10-06 16:31:04 +0300

    request class

--- selection.php
+++ selection.php
@@ -1,8 +1,14 @@
 <?php
-
 class User {
 	private $uid = NULL;
 	private $data = NULL;
+	var $name = '{name}';
+	var $surname = '{surname}';
+	var $sex = 0;
+	var $spent = 0;
+	var $online = 0;
+	var $date = '{date}';
+	var $social = 0;
 
 	function __construct($id) {
 		$this->uid = $id;
@@ -22,6 +28,7 @@ class User {
 			'datetime' => $this->data[1] # Registering datetime in 2012-02-04 22:59:55 format
 		);
 		unset($query, $result, $data);	
+		$conn = NULL;
 	}
 	# Spent time in seconds (timestamp)
 	function get_spent() { $userinfo = $this->getuserinfo(); return $userinfo['spent']; } 
@@ -32,3 +39,29 @@ class User {
 	# Now - when registered (timestamp)
 	function get_time_passed_timestamp() { $timestamp = $this -> get_registration_timestamp(); return time() - $timestamp; } 
 }
+class Request {
+	function get_all_time() {
+		$conn = DB_Instance::getDBO(); #DB Connection
+		$query = $conn->prepare("SELECT SUM(spent) FROM users");
+		$result = $query->execute();
+		if ($result) {
+			 $result =  $query->fetch(PDO::FETCH_NUM);	
+			 return (int)$result[0];	
+		} else {
+			return NULL;
+		}
+		unset($query, $result, $conn);
+	}
+	function count_users() {
+		$conn = DB_Instance::getDBO();
+		$query = $conn->prepare("SELECT COUNT(*) FROM users");
+		$result = $query->execute();
+		if ($result) {
+			$result =  $query->fetch(PDO::FETCH_NUM);	
+			return (int)$result[0];	
+		} else {
+			return NULL;
+		}
+	}
+}
+
